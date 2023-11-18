785bd05dc86826807d3ca54b908daae4efb9ecbd
KIM <hako5460@naver.com>
2023-11-18 20:46:38 +0900

    lint 무시

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
   </nav>
-  <router-view/>
 </template>
+
+<script>
+// @ is an alias to /src
+export default {
+  components: {
+  }
+}
+</script>
 
 <style>
 #app {
