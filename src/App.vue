e31a742832c58a43a8646ac6c38245848184893d
KIM <hako5460@naver.com>
2023-11-21 23:58:35 +0900

    탭UI 생성

--- src/App.vue
+++ src/App.vue
@@ -9,32 +9,47 @@
     <img src="./assets/logo.png" class="logo" />
   </div>
 
-  <Container :데이터목록 = "데이터목록"/>
-
+  <Container :데이터목록="데이터목록" />
+  <button @click="more">더보기</button>
   <div class="footer">
     <ul class="footer-button-plus">
       <input type="file" id="file" class="inputfile" />
       <label for="file" class="input-plus">+</label>
     </ul>
   </div>
+
 </template>
 
 <script>
-import Data from './assets/data.js';
-import Container from './components/Container.vue';
+import { tSMethodSignature, thisTypeAnnotation } from "@babel/types";
+import Data from "./assets/data.js";
+import Container from "./components/Container.vue";
+import axios from "axios";
+
 // @ is an alias to /src
 export default {
   data() {
     return {
-      데이터목록: Data
-    }
+      데이터목록: Data,
+      더보기클릭횟수: 0,
+    };
   },
   components: {
     Container,
+  },
+  methods: {
+    more() {
+        axios
+          .get(`https://codingapple1.github.io/vue/more${this.더보기클릭횟수}.json`)
+          .then((result) => {
+            this.데이터목록.push(result.data);
+            this.더보기클릭횟수 += 1;
+          });
+    },
   },
 };
 </script>
 
 <style>
-@import 'style.css';
+@import "style.css";
 </style>
--- src/components/Container.vue
+++ src/components/Container.vue
@@ -1,23 +1,40 @@
 <template>
   <div>
-    <Post v-for="(게시물, index) in 데이터목록" :key="index" :게시물="게시물"/>
+    <!-- 저번시간 까지 -->
+    <Post v-for="(게시물, index) in 데이터목록" :key="index" :게시물="게시물" />
+
+    <!-- 필터선택페이지 -->
+    <div class="upload-image"></div>
+    <div class="filters">
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+      <div class="filter-1"></div>
+    </div>
+
+    <!-- 글작성페이지 -->
+    <div class="upload-image"></div>
+    <div class="write">
+      <textarea class="write-box">write!</textarea>
+    </div>
   </div>
 </template>
 
 <script>
-import Post from './Post.vue';
+import Post from "./Post.vue";
 export default {
   data() {
     return {
       데이터목록: this.데이터목록,
-    }
+    };
   },
   components: {
     Post,
   },
   props: {
     데이터목록: Array,
-  }
+  },
 };
 </script>
 
